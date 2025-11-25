76c80ec53cdabba9c1828e38867264de3b855630
asistenteconta-blip <asistenteconta@casaaguizotes.com>
2025-11-25 15:12:59 -0600

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -346,8 +346,8 @@ previo["VALOR INVENTARIO (PREVIO)"] = previo["VALOR INVENTARIO (PREVIO)"].round(
 st.dataframe(
     previo[[
         "PRODUCTO",
-        "CANTIDAD CERRADO",
-        "CANTIDAD ABIERTO (PESO)",
+        "CERRADO",
+        "ABIERTO (PESO)",
         "VALOR INVENTARIO (PREVIO)",
     ]],
     use_container_width=True,
@@ -544,6 +544,7 @@ if st.session_state.get("confirm_reset", False):
         if st.button("❌ Cancelar"):
             st.info("Operación cancelada, no se modificó nada.")
             st.session_state["confirm_reset"] = False
+
 
 
 
