cc037883e25266158937c6e45889646c3af226b8
asistenteconta-blip <asistenteconta@casaaguizotes.com>
2025-12-03 09:01:36 -0600

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -154,21 +154,19 @@ df_edit = st.data_editor(
     column_config={
         "CERRADO": st.column_config.NumberColumn(
             "CERRADO",
-            format="%.2f",
             step=0.1
         ),
         "ABIERTO(PESO)": st.column_config.NumberColumn(
             "ABIERTO (PESO)",
-            format="%.2f",
             step=0.1
         ),
         "BOTELLAS_ABIERTAS": st.column_config.NumberColumn(
             "BOTELLAS ABIERTAS",
-            format="%.2f",
             step=1
         ),
     }
 )
+
 
 
 # =========================================================
@@ -312,6 +310,7 @@ if st.button("💬 Guardar comentario"):
     ws = get_sheet(area)
     ws.update("C3", [[st.session_state["comentario"]]])
     st.success("Comentario guardado ✔")
+
 
 
 
